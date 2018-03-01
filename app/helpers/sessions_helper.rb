08512534fe4f4d92fc44a3dc34ce7de47365f9d7
pinkal-bacancy <pinkal.darji@bacancytechnology.com>
2018-03-01 18:25:34 +0530

    User edit,delete,index performed

--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -1,4 +1,4 @@
-module SessionsHelper
+  module SessionsHelper
   def log_in(user)
     session[:user_id] = user.id
   end
@@ -24,6 +24,18 @@ module SessionsHelper
       end
     end
 
+  end
+   def redirect_back_or(default)
+    redirect_to(session[:forwarding_url] || default)
+    session.delete(:forwarding_url)
+  end
+
+  # Stores the URL trying to be accessed.
+  def store_location
+    session[:forwarding_url] = request.original_url if request.get?
+  end
+  def current_user?(user)
+    user == current_user
   end
   def logged_in?
     !current_user.nil?
